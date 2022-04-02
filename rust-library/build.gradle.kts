a2448985df34628cd532818ebacf123f15b7b06c
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-04-02 23:01:17 +0200

    project: gradle-rust v3.2.0

--- rust-library/build.gradle.kts
+++ rust-library/build.gradle.kts
@@ -9,29 +9,35 @@ rust {
     // you can also use tools like "cross"
     command.set("cross")
 
+    // this will make cargo install automatically the required targets
+    // `rustup targets add x86_32-pc-windows-gnu`
+    cargoInstallTargets.set(true)
+
     // Adding a simple target with default options
-    target("x86_32-pc-windows-gnu" to "test.dll")
+    targets += target("i686-pc-windows-gnu", "test.dll")
 
     // Defining per-targets
     targets {
         // Adds the default target
-        this += defaultTarget()
+        this += defaultTarget().apply {
+            command = "cargo"
+        }
 
         // Creates a target with a custom file output
-        create("win32") {
+        create("win64") {
             target = "x86_64-pc-windows-gnu"
             outputName = "test64.dll"
         }
 
         // Custom target with different params than default
-        create("macOS") {
-            target = "x86_64-apple-darwin"
-            outputName = "libtest64.dylib"
-
-            release = false
-
-            // Fallback to cargo
-            command = "cargo"
-        }
+//        create("macOS") {
+//            target = "x86_64-apple-darwin"
+//            outputName = "libtest64.dylib"
+//
+//            release = false
+//
+//            // Fallback to cargo
+//            command = "cargo"
+//        }
     }
 }
