95a81844f64fed408b677c454d8ed645588fb0ff
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-04-02 02:38:56 +0200

    project: added samples for gradle-rust v3.2.0/next

--- rust-library/build.gradle.kts
+++ rust-library/build.gradle.kts
@@ -1,11 +1,37 @@
 plugins {
-    id("fr.stardustenterprises.rust.wrapper") version "3.1.0"
+    id("fr.stardustenterprises.rust.wrapper") version "3.2.0"
 }
 
 rust {
-    // you can also use tools like "cross"
-    command.set("cargo")
+    // Setting the global "default" vars
+    release.set(true)
 
-    targets += defaultTarget()
-    targets += "x86_64-pc-windows-gnu" to "test64.dll"
+    // you can also use tools like "cross"
+    command.set("cross")
+
+    // Adding a simple target with default options
+    target("x86_32-pc-windows-gnu" to "test.dll")
+
+    // Defining per-targets
+    targets {
+        // Adds the default target
+        this += defaultTarget()
+
+        // Creates a target with a custom file output
+        create("win32") {
+            target = "x86_64-pc-windows-gnu"
+            outputName = "test64.dll"
+        }
+
+        // Custom target with different params than default
+        create("macOS") {
+            target = "x86_64-apple-darwin"
+            outputName = "libtest64.dylib"
+
+            release = false
+
+            // Fallback to cargo
+            command = "cargo"
+        }
+    }
 }
